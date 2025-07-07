a687e6676f5268eb6683a25f3855a314eef8c83e
analiria <pires.portes@escola.pr.gov.br>
2025-07-07 11:08:17 -0300

    ferias

--- main.js
+++ main.js
@@ -4,7 +4,15 @@ for(let i = 0; i<botoes.length; i++){
     botoes[i].onclick = function(){
         for(let j = 0; i < botoes;length; j++){
             botoes[j].classList.remove("ativo")
+            textos[j].classList.remove("ativo")
         }
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
 }
+
+const contadores = document.querySelectorAll("contador");
+const tempoObjetivo1 = new date("2025-10-05t00:00:00");
+let   tempoAtual = new date();
+
+contadores[0].textContent = tenpoObjetivo1 - tempoAtual;
